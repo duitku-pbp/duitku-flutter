7d97e657e4c09b178bd93f7dfd76eecc2834081b
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-11-29 17:41:52 +0700

    feat: add get wallet response model

---
+++ lib/wallet/messages/get_wallet_response.dart
@@ -0,0 +1,15 @@
+import 'package:duitku/wallet/models/wallet.dart';
+
+class GetWalletResponse {
+  final List<Wallet> wallets;
+
+  GetWalletResponse({required this.wallets});
+
+  factory GetWalletResponse.fromJson(List<Map<String, dynamic>> json) =>
+      GetWalletResponse(
+        wallets: List<Wallet>.from(json.map((map) => Wallet.fromMap(map))),
+      );
+
+  List<Map<String, dynamic>> toJson() =>
+      wallets.map((wallet) => wallet.toMap()).toList();
+}
