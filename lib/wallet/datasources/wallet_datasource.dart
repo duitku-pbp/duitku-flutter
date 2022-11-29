f6db34f9c3d47ee497448d7e291a2a7ec7e02eea
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-11-29 22:42:50 +0700

    feat: implement get report repository and datasource handler

--- lib/wallet/datasources/wallet_datasource.dart
+++ lib/wallet/datasources/wallet_datasource.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:duitku/common/constants.dart';
 import 'package:duitku/common/exceptions.dart';
+import 'package:duitku/wallet/messages/get_report_response.dart';
 import 'package:duitku/wallet/messages/get_wallet_response.dart';
+import 'package:duitku/wallet/models/report.dart';
 import 'package:duitku/wallet/models/wallet.dart';
 import 'package:http/http.dart' as http;
 
@@ -28,8 +30,25 @@ class WalletDatasource {
 
     if (res.statusCode == 200) {
       return GetWalletResponse.fromJson(json.decode(res.body)).wallets;
-    } else {
-      throw HttpException();
     }
+
+    throw HttpException();
+  }
+
+  Future<Report> getReport(String period) async {
+    final uri = Uri.parse("$baseUrl/wallet/api/report/$period");
+    final cookies = await jar.loadForRequest(uri);
+    final sessionId = cookies.where((c) => c.name == "sessionid").join();
+
+    final res = await client.get(
+      uri,
+      headers: {"Cookie": sessionId.toString()},
+    );
+
+    if (res.statusCode == 200) {
+      return GetReportResponse.fromJson(json.decode(res.body)).report;
+    }
+
+    throw HttpException();
   }
 }
--- lib/wallet/repositories/wallet_repository.dart
+++ lib/wallet/repositories/wallet_repository.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:duitku/common/exceptions.dart';
 import 'package:duitku/common/failures.dart';
 import 'package:duitku/wallet/datasources/wallet_datasource.dart';
+import 'package:duitku/wallet/models/report.dart';
 import 'package:duitku/wallet/models/wallet.dart';
 
 class WalletRepository {
@@ -12,6 +13,15 @@ class WalletRepository {
   Future<Either<Failure, List<Wallet>>> getWallets() async {
     try {
       final res = await datasource.getWallets();
+      return Right(res);
+    } on HttpException {
+      return Left(HttpFailure("An error occured"));
+    }
+  }
+
+  Future<Either<Failure, Report>> getReport(String period) async {
+    try {
+      final res = await datasource.getReport(period);
       return Right(res);
     } on HttpException {
       return Left(HttpFailure("An error occured"));
