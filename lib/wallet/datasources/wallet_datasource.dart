68773ce08a3df636daf5e3eb7832d996a8f93312
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-01 01:59:01 +0700

    feat: implement get transactions handler in datasource

--- lib/wallet/datasources/wallet_datasource.dart
+++ lib/wallet/datasources/wallet_datasource.dart
@@ -4,8 +4,10 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:duitku/common/constants.dart';
 import 'package:duitku/common/exceptions.dart';
 import 'package:duitku/wallet/messages/get_report_response.dart';
+import 'package:duitku/wallet/messages/get_transactions_response.dart';
 import 'package:duitku/wallet/messages/get_wallet_response.dart';
 import 'package:duitku/wallet/models/report.dart';
+import 'package:duitku/wallet/models/transaction_group.dart';
 import 'package:duitku/wallet/models/wallet.dart';
 import 'package:http/http.dart' as http;
 
@@ -47,6 +49,28 @@ class WalletDatasource {
 
     if (res.statusCode == 200) {
       return GetReportResponse.fromJson(json.decode(res.body)).report;
+    }
+
+    throw HttpException();
+  }
+
+  Future<List<TransactionGroup>> getTransactions([
+    String wallet = "all",
+  ]) async {
+    final uri =
+        Uri.parse("$baseUrl/wallet/api/transaction/?from-wallet=$wallet");
+    final cookies = await jar.loadForRequest(uri);
+    final sessionId = cookies.where((c) => c.name == "sessionid").join();
+
+    final res = await client.get(
+      uri,
+      headers: {"Cookie": sessionId.toString()},
+    );
+
+    if (res.statusCode == 200) {
+      return GetTransactionsResponse.fromJson(
+        json.decode(res.body),
+      ).transactionGroups;
     }
 
     throw HttpException();
