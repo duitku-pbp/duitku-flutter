8951940948b6a977c928d56420f0fc071ca801ea
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-01 02:04:13 +0700

    feat: implement get transactions in wallet provider

--- lib/wallet/providers/wallet_provider.dart
+++ lib/wallet/providers/wallet_provider.dart
@@ -1,17 +1,21 @@
 import 'package:duitku/wallet/models/report.dart';
+import 'package:duitku/wallet/models/transaction_group.dart';
 import 'package:duitku/wallet/models/wallet.dart';
 import 'package:duitku/wallet/repositories/wallet_repository.dart';
 import 'package:flutter/foundation.dart';
 
 class WalletProvider with ChangeNotifier {
   final WalletRepository repository;
+
   List<Wallet> _wallets = [];
   Report? _report;
+  List<TransactionGroup> _transactionGroups = [];
 
   WalletProvider({required this.repository});
 
   List<Wallet> get wallets => _wallets;
   Report? get report => _report;
+  List<TransactionGroup> get transactionGroups => _transactionGroups;
 
   Future<void> getWallets() async {
     final res = await repository.getWallets();
@@ -28,6 +32,18 @@ class WalletProvider with ChangeNotifier {
     res.fold(
       (failure) {},
       (report) => _report = report,
+    );
+
+    notifyListeners();
+  }
+
+  Future<void> getTransactions([
+    String wallet = "all",
+  ]) async {
+    final res = await repository.getTransactions(wallet);
+    res.fold(
+      (failure) {},
+      (trxGroups) => _transactionGroups = trxGroups,
     );
 
     notifyListeners();
