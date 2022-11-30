baac53fa7bb6b04c1027d5817fe8f7911018b77b
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-01 01:47:30 +0700

    feat: implement transaction group model

---
+++ lib/wallet/models/transaction_group.dart
@@ -0,0 +1,27 @@
+import 'package:duitku/wallet/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionGroup {
+  DateTime date;
+  List<Transaction> transactions;
+
+  final _dateFormatter = DateFormat("yyyy-MM-dd");
+
+  TransactionGroup({
+    required this.date,
+    required this.transactions,
+  });
+
+  factory TransactionGroup.fromMap(Map<String, dynamic> map) =>
+      TransactionGroup(
+        date: DateTime.tryParse(map["date"]) ?? DateTime(1970, 1, 1),
+        transactions: List<Transaction>.from(
+          map["transactions"].map((trx) => Transaction.fromMap(trx)),
+        ),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "date": _dateFormatter.format(date),
+        "transactions": transactions.map((trx) => trx.toMap()).toList(),
+      };
+}
