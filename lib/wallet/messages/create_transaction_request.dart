39314a7292e0aabd25bb30309c6cb35ae9feec8d
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-01 18:14:11 +0700

    feat: add create transactionr request message

---
+++ lib/wallet/messages/create_transaction_request.dart
@@ -0,0 +1,28 @@
+import 'package:duitku/wallet/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class CreateTransactionRequest {
+  int walletId;
+  double amount;
+  TransactionType type;
+  DateTime doneOn;
+  String description;
+
+  final _dateFormatter = DateFormat("yyyy-MM-dd");
+
+  CreateTransactionRequest({
+    required this.walletId,
+    required this.amount,
+    required this.type,
+    required this.doneOn,
+    required this.description,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "wallet": walletId,
+        "amount": amount,
+        "type": type.toString(),
+        "done_on": _dateFormatter.format(doneOn),
+        "description": description,
+      };
+}
