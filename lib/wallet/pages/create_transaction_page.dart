d8569e62c0f2be4cbf81ae2b45506281247e786f
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-02 01:11:57 +0700

    feat: finish create transaction page UI

--- lib/wallet/pages/create_transaction_page.dart
+++ lib/wallet/pages/create_transaction_page.dart
@@ -3,6 +3,7 @@ import 'package:duitku/wallet/models/transaction.dart';
 import 'package:duitku/wallet/pages/transactions_page.dart';
 import 'package:duitku/wallet/providers/wallet_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class CreateTransactionPage extends StatefulWidget {
@@ -15,40 +16,305 @@ class CreateTransactionPage extends StatefulWidget {
 }
 
 class _CreateTransactionPageState extends State<CreateTransactionPage> {
+  final GlobalKey<FormState> _createTransactionFormKey = GlobalKey();
+  final now = DateTime.now();
+  final firstDate = DateTime(1970, 1, 1);
+
+  final _dateFormatter = DateFormat("yyyy-MM-dd");
+  final TextEditingController _doneOnController = TextEditingController();
+
   WalletProvider? walletProv;
+
+  int? _walletId;
+  double? _amount;
+  TransactionType? _type;
+  DateTime? _doneOn;
+  String? _description;
 
   @override
   void initState() {
     walletProv = Provider.of<WalletProvider>(context, listen: false);
+    walletProv?.getWallets();
     super.initState();
+  }
+
+  Future<void> _createTransaction() async {
+    if (_createTransactionFormKey.currentState!.validate()) {
+      if (walletProv?.createTransactionRequestState ==
+          CreateTransactionRequestState.ok) {
+        walletProv?.resetStates();
+
+        Navigator.of(context).pushReplacementNamed(TransactionsPage.routeName);
+      } else {}
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final lastDate =
+        DateTime(now.year, now.month + 1, 1).subtract(const Duration(days: 1));
+
     return Scaffold(
       appBar: AppBar(title: const Text("Add Transaction")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await walletProv?.createTransaction(
-              body: CreateTransactionRequest(
-                walletId: 1,
-                type: TransactionType.income,
-                amount: 1000,
-                doneOn: DateTime.now(),
-                description: "Test",
+      body: Form(
+        key: _createTransactionFormKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text(
+                  "Create New Transaction",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            );
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Description",
+                              hintText: "ex: Lunch",
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              contentPadding: const EdgeInsets.all(10),
+                            ),
+                            onChanged: (String? val) {
+                              setState(() {
+                                _description = val;
+                              });
+                            },
+                            onSaved: (String? val) {
+                              setState(() {
+                                _description = val;
+                              });
+                            },
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return "Please enter a description";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 25),
+                          TextFormField(
+                            keyboardType: const TextInputType.numberWithOptions(
+                              signed: false,
+                              decimal: true,
+                            ),
+                            decoration: InputDecoration(
+                              labelText: "Amount (Rp.)",
+                              hintText: "ex: 1000",
+                              contentPadding: const EdgeInsets.all(10),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                            ),
+                            onChanged: (String? val) {
+                              setState(() {
+                                _amount = double.tryParse(val ?? "");
+                              });
+                            },
+                            onSaved: (String? val) {
+                              setState(() {
+                                _amount = double.tryParse(val ?? "");
+                              });
+                            },
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return "Please enter an amount for the transaction";
+                              } else if (double.tryParse(val) == null) {
+                                return "Amount must be numeric";
+                              } else if (double.parse(val) <= 0) {
+                                return "Amount must be a positive number";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 25),
+                          TextFormField(
+                            readOnly: true,
+                            controller: _doneOnController,
+                            keyboardType: TextInputType.datetime,
+                            onTap: () async {
+                              FocusScope.of(context).requestFocus(FocusNode());
 
-            if (mounted &&
-                walletProv?.createTransactionRequestState ==
-                    CreateTransactionRequestState.ok) {
-              walletProv?.resetStates();
-              Navigator.of(context)
-                  .pushReplacementNamed(TransactionsPage.routeName);
-            }
-          },
-          child: const Text("Create"),
+                              final selectedDate = await showDatePicker(
+                                context: context,
+                                initialDate: now,
+                                firstDate: firstDate,
+                                lastDate: lastDate,
+                                keyboardType: TextInputType.datetime,
+                              );
+
+                              if (selectedDate != null) {
+                                setState(() {
+                                  _doneOnController.text =
+                                      _dateFormatter.format(selectedDate);
+                                  _doneOn = selectedDate;
+                                });
+                              }
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Done on",
+                              hintText: "ex: 1970-01-01",
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              contentPadding: const EdgeInsets.all(10),
+                            ),
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return "Please enter a date for the transaction";
+                              } else if (DateTime.tryParse(val) == null) {
+                                return "Date is not valid";
+                              } else if (DateTime.parse(val)
+                                      .isAtSameMomentAs(lastDate) ||
+                                  DateTime.parse(val).isAfter(lastDate)) {
+                                return "Can only enter dates up until the current month";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 25),
+                          DropdownButtonHideUnderline(
+                            child: DropdownButtonFormField(
+                              decoration: InputDecoration(
+                                labelText: "Type",
+                                hintText: "Select a transaction type",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                contentPadding: const EdgeInsets.all(10),
+                              ),
+                              value: _type,
+                              items: [
+                                DropdownMenuItem(
+                                  value: TransactionType.income,
+                                  child: Text(TransactionType.income.text),
+                                ),
+                                DropdownMenuItem(
+                                  value: TransactionType.outcome,
+                                  child: Text(TransactionType.outcome.text),
+                                )
+                              ],
+                              onChanged: (val) {
+                                setState(() {
+                                  _type = val;
+                                });
+                              },
+                              onSaved: (val) {
+                                setState(() {
+                                  _type = val;
+                                });
+                              },
+                              validator: (val) {
+                                if (val == null) {
+                                  return "Please select a transaction type";
+                                } else if (val != TransactionType.income &&
+                                    val != TransactionType.outcome) {
+                                  return "Not a valid transaction type";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 25),
+                          DropdownButtonHideUnderline(
+                            child: DropdownButtonFormField(
+                              decoration: InputDecoration(
+                                labelText: "Wallet",
+                                hintText: "Select a wallet",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                contentPadding: const EdgeInsets.all(10),
+                              ),
+                              value: _walletId,
+                              items: walletProv != null &&
+                                      walletProv!.wallets.isNotEmpty
+                                  ? walletProv!.wallets
+                                      .map((wallet) => DropdownMenuItem(
+                                            value: wallet.id,
+                                            child: Text(
+                                              "${wallet.name} (${wallet.balance})",
+                                            ),
+                                          ))
+                                      .toList()
+                                  : const [
+                                      DropdownMenuItem(
+                                        value: -1,
+                                        child: Text("Select a wallet"),
+                                      )
+                                    ],
+                              onChanged: (val) {
+                                setState(() {
+                                  _walletId = val;
+                                });
+                              },
+                              onSaved: (val) {
+                                setState(() {
+                                  _walletId = val;
+                                });
+                              },
+                              validator: (val) {
+                                if (val == null) {
+                                  return "Please select a wallet";
+                                } else if (val == -1) {
+                                  return "No wallets found";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 25),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: _createTransaction,
+                                  style: ElevatedButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 16,
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "Create",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
