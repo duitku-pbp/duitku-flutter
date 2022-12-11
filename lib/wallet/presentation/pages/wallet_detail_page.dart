500f879c564863e3b0b6cdcc343fda1b7b267839
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-11 20:48:36 +0700

    feat: add delete button to wallet detail page

--- lib/wallet/presentation/pages/wallet_detail_page.dart
+++ lib/wallet/presentation/pages/wallet_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:duitku/wallet/presentation/bloc/providers/wallet_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class WalletDetailPage extends StatefulWidget {
@@ -12,6 +13,11 @@ class WalletDetailPage extends StatefulWidget {
 }
 
 class _WalletDetailPageState extends State<WalletDetailPage> {
+  final _currencyFormatter = NumberFormat.currency(
+    symbol: "Rp",
+    locale: "id-ID",
+  );
+
   late int _walletId;
 
   WalletProvider? _walletProv;
@@ -33,7 +39,63 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Wallet Detail")),
+      appBar: AppBar(
+        title: const Text("Wallet Detail"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text(
+                    "Delete Wallet",
+                    style: TextStyle(fontSize: 24),
+                    textAlign: TextAlign.center,
+                  ),
+                  titlePadding: const EdgeInsets.symmetric(vertical: 18),
+                  contentPadding: const EdgeInsets.all(12),
+                  content: SizedBox(
+                    height: 120,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              "Are you sure you want to delete your ${_walletProv!.wallet!.name} wallet?",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("No"),
+                              ),
+                              ElevatedButton(
+                                onPressed: () async {},
+                                child: const Text("Yes"),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
       body: RefreshIndicator(
         onRefresh: () async {
           await _walletProv?.getWalletDetail(_walletId);
@@ -49,7 +111,35 @@ class _WalletDetailPageState extends State<WalletDetailPage> {
                 return const Center(child: Text("Failed to get wallet"));
               }
 
-              return const SizedBox();
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _walletProv!.wallet!.name,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Text(
+                        _currencyFormatter.format(_walletProv!.wallet!.balance),
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      const SizedBox(height: 15),
+                      Text(
+                        _walletProv!.wallet!.description,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             }),
       ),
     );
