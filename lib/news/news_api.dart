d91d84b75bc5fafeaf1536762e89c6a9cd127b6f
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-12 23:30:28 +0700

    fix: fix analyzer

--- lib/news/news_api.dart
+++ lib/news/news_api.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 //import 'dart:html';
 import 'package:duitku/news/news_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 // class newsAPI -> disini adalah tempat untuk memanggil API dan parameter API ditulis
@@ -28,4 +27,3 @@ class NewsApi {
     return News.newsFromSnapshot(temp);
   }
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,10 @@ import Foundation
 
 import shared_preferences_macos
 import url_launcher_macos
-<<<<<<< HEAD
-=======
 import wakelock_macos
->>>>>>> c986b058cd69b2bacdd68b547bdee49173161701
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-<<<<<<< HEAD
-=======
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
->>>>>>> c986b058cd69b2bacdd68b547bdee49173161701
 }
